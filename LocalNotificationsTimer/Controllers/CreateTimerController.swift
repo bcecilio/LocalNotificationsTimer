b2c8a2403e7b014f42ae58072c7ea84eaa868863
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-20 14:57:02 -0500

    added storyboard elements and pickerView methods in extensions

--- LocalNotificationsTimer/Controllers/CreateTimerController.swift
+++ LocalNotificationsTimer/Controllers/CreateTimerController.swift
@@ -9,22 +9,63 @@
 import UIKit
 
 class CreateTimerController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var hour:Int = 0
+    var minutes:Int = 0
+    var seconds:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension CreateTimerController: UIPickerViewDelegate,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
     }
-    */
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 25
+        case 1, 2:
+            return 60
 
+        default:
+            return 0
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width / 3
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row) Hr"
+        case 1:
+            return "\(row) Min"
+        case 2:
+            return "\(row) Sec"
+        default:
+            return ""
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            hour = row
+        case 1:
+            minutes = row
+        case 2:
+            seconds = row
+        default:
+            break;
+        }
+    }
 }
